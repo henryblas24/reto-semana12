1166908b0cb6e13367a22cc2b95f9adf02a162cc
henry blas <hblas2014@gmail.com>
2023-06-10 10:46:57 -0500

    se termino reto 12

--- app2.js
+++ app2.js
@@ -5,10 +5,21 @@ const modal = document.querySelector('.modal')
 boton.addEventListener('click',(e) => {
 modal.style='display: block'} )
 
+const cerrar = document.querySelector('.cerrar')
+
+cerrar.addEventListener('click',(e) => {
+    modal.style='display: none'} )
 
 const aceptar = document.getElementById('aceptar')
 console.log(aceptar)
 
+aceptar.addEventListener('click',(e) => {
+    modal.style='display: none'} )
+
 
 const cancel = document.getElementById('cancel')
 console.log(cancel)
+
+cancel.addEventListener('click',(e) => {
+    modal.style='display: none'} )
+    
